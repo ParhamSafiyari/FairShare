8f84664b7226085571aad2f1cc73807898ee0dad
Parham Safiyari <safiyari18@gmail.com>
2026-07-26 18:50:13 +0330

    Add itemized expenses so specific purchases can be split among a subset of people

--- web/service-worker.js
+++ web/service-worker.js
@@ -1,7 +1,7 @@
 // FairShare service worker
 // Caches the app shell so it works offline and loads instantly once installed.
 
-const CACHE_NAME = 'fairshare-v1.0.3';
+const CACHE_NAME = 'fairshare-v1.0.4';
 const APP_SHELL = [
   './index.html',
   './manifest.json',
