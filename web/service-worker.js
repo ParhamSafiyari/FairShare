7dfea997d267c6459d96aad0880e07a4b247cab6
Parham Safiyari <safiyari18@gmail.com>
2026-08-01 17:04:10 +0330

    Add apikey header to Edge Function requests and fix some noises in chrome browser

--- web/service-worker.js
+++ web/service-worker.js
@@ -60,11 +60,13 @@ self.addEventListener('fetch', (event) => {
 
       return fetch(event.request)
         .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          if (event.request.url.startsWith('http')) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          }
           return response;
         })
-        .catch(() => cached);
+        .catch(() => caches.match(event.request))
     })
   );
 });
